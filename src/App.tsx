350ffd2eb1e21ac31d41eacd601c6fd1e225a75a
Glenn <phoobynet@users.noreply.github.com>
2022-12-17 11:34:38 +0000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Home from './routes/Home'
+import SignUpForm from './routes/SignUpForm'
 import { Route, Routes } from 'react-router-dom'
 
-export default function App() {
+export default function App () {
   return (
     <Routes>
       <Route
         index
-        element={<Home />}
+        element={<SignUpForm />}
       />
     </Routes>
   )
---
+++ src/routes/SignUpForm.tsx
@@ -0,0 +1,8 @@
+import styles from './SignUpForm.module.scss'
+
+export default function SignUpForm () {
+  return (
+    <div className={styles.signUpForm}>
+    </div>
+  )
+}
